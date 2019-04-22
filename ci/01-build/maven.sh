86878cd0682b59548a053635287b1a44012f1f88
francisco <fmcejudo@gmail.com>
2019-04-22 22:55:22 +0200

    Removing from script the starting point of docker

--- ci/01-build/maven.sh
+++ ci/01-build/maven.sh
@@ -4,8 +4,8 @@
 cd source_code || echo "missing input source_code: source_code"
 
 #echo "Using MAVEN_OPTS: ${MAVEN_OPTS}"
-source /docker-lib.sh
-start_docker
+#source /docker-lib.sh
+#start_docker
 
 mvn compile test ${MAVEN_ARGS}
 
